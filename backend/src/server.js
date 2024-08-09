54a56011862f1e416135a289ab06e20e3d783c96
João Victor <joaocrivoi13@gmail.com>
2024-08-09 15:36:26 -0300

    Algumas Configuração do Ambiente de Desenvolvimento

---
+++ backend/src/server.js
@@ -0,0 +1,49 @@
+import fastify from "fastify"
+import { prisma } from "./lib/prisma";
+import { createTrip } from "./routes/create-trip";
+import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import { confirmTrip } from "./routes/confirm-trip";
+import cors from '@fastify/cors'
+import { confirmParticipants } from "./routes/confirm-participant";
+import { createActivity } from "./routes/create-activity";
+import { getActivities} from "./routes/get-activities";
+import { createLink } from "./routes/create-link";
+import { getLinks } from "./routes/get-links";
+import { getParticipants } from "./routes/get-participants";
+import { createInvites } from "./routes/create-invite";
+import { updateTrip } from "./routes/update-trip";
+import { getTripDetails } from "./routes/get-trip-details";
+import { getParticipant } from "./routes/get-participant";
+import { errorHandler } from "./error-handler";
+import { env } from "./env";
+
+const app = fastify()
+
+app.register(cors, {
+    origin: '*'
+})
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.setErrorHandler(errorHandler)
+
+app.register(createTrip)
+app.register(confirmTrip)
+app.register(confirmParticipants)
+app.register(createActivity)
+app.register(getActivities)
+app.register(createLink)
+app.register(getLinks)
+app.register(getParticipants)
+app.register(createInvites)
+app.register(updateTrip)
+app.register(getTripDetails)
+app.register(getParticipant)
+
+
+
+app.listen({ port: env.PORT}).then(()=>{
+    console.log('Server running')
+})
+
