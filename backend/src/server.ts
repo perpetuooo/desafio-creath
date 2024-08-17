f3114354b67566b788df0e446d17c8ec08addb35
Pedro <pedrohenriqueperpetuo@gmail.com>
2024-08-17 12:59:55 -0300

    init db & typescript config

---
+++ backend/src/server.ts
@@ -0,0 +1,42 @@
+import Fastify, { FastifyReply, FastifyRequest } from "fastify"
+import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import cors from '@fastify/cors'
+
+const app = Fastify({ logger: true })
+
+app.register(cors, {
+    origin: '*'
+})
+
+async function main() {
+    app.setValidatorCompiler(validatorCompiler)
+    app.setSerializerCompiler(serializerCompiler)
+    
+    app.get('/', (req: FastifyRequest, rep: FastifyReply) => {
+        rep.send({ message: "Barba, Cabelo e Bigode." })
+    })
+    
+    const port = process.env.PORT ? parseInt(process.env.PORT) : 8080
+    const host = process.env.HOST || '0.0.0.0'
+    
+    await app.listen({ 
+        port: port,
+        host: host 
+    }).then(()=>{
+        console.log('Server running at port ' + port)
+    }).catch(err => {
+        console.error(err)
+        process.exit(1)
+    })
+}
+
+main()
+
+process.on('SIGINT', async () => {
+    process.exit()
+  })
+  
+  process.on('SIGTERM', async () => {
+    process.exit()
+  })
+  
